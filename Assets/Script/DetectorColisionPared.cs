6d55608f674cf59ca7c94609ee5145b7fcf228b4
LilianXeyman <elisa0101232478@gmail.com>
2024-09-28 14:20:38 +0100

    Video 4
    Paredes que cambian de color con el impacto (5s)

---
+++ Assets/Script/DetectorColisionPared.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectorColisionPared : MonoBehaviour
+{
+    [SerializeField]
+    Material newMaterial2;
+
+    [SerializeField]
+    Material paredTocada;
+
+    bool paredVioleta = false;
+
+    float tiempoVioleta = 5f;
+    //Funcion que se ejecuta cada vez por imagen del juego
+    private void Update()
+    {
+        if(paredVioleta==true)
+        {
+            tiempoVioleta = tiempoVioleta - Time.deltaTime;
+            if (tiempoVioleta < 0.0f)
+            {
+                gameObject.GetComponent<MeshRenderer>().material = newMaterial2;
+                paredVioleta = false;
+                tiempoVioleta = 5f;
+            }
+        }
+    }
+    //Funcion que se ejecuta cada vez que haya una colision con este gameObject
+    private void OnCollisionEnter(Collision collision)
+    {
+      
+        if(collision.gameObject.tag == "Player")
+        {
+         Debug.Log(collision.gameObject.name);
+            gameObject.GetComponent<MeshRenderer>().material = paredTocada;
+            paredVioleta = true;
+        }
+    }
+
+}
