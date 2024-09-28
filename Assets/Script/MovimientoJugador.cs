ae6b9c7d597d0361d38f620428de6b225681af15
LilianXeyman <elisa0101232478@gmail.com>
2024-09-28 13:27:29 +0100

    Video 3
    Movimiento en los ejes (x,z)

--- Assets/Script/MovimientoJugador.cs
+++ Assets/Script/MovimientoJugador.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +8,7 @@ public class MovimientoJugador : MonoBehaviour
     public float movimientoX;
     public float movimientoY;
     public float movimientoZ;
-
+    public float velocidadMovimiento = 1.5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +18,8 @@ public class MovimientoJugador : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        movimientoX=-Input.GetAxis("Horizontal")*Time.deltaTime*velocidadMovimiento;
+        movimientoZ = -Input.GetAxis("Vertical")*Time.deltaTime*velocidadMovimiento;
         transform.Translate(movimientoX, movimientoY, movimientoZ);
     }
 }
