56e2e04d864cb271f1e48edb6d4f35691e98d218
LilianXeyman <elisa0101232478@gmail.com>
2024-09-28 23:19:21 +0100

    Video 6
    Intenté hacer el video 6 pero la pantalla me sale con el mensaje de has ganado desde el principio.

---
+++ Assets/Script/DetectorMeta.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectorMeta : MonoBehaviour
+{
+    [SerializeField]
+    GameObject pantallaFinal;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        
+        if (other.tag == "Player")
+        {
+            Debug.Log("Has llegado a la meta");
+
+            pantallaFinal.SetActive(true);
+        }
+    }  
+}
