e504f13e5d84039828d6811a6c8c733b40f75455
LilianXeyman <elisa0101232478@gmail.com>
2024-09-29 14:42:39 +0100

    Video 7
    Pantalla con el contador de tiempo

--- Assets/Script/DetectorMeta.cs
+++ Assets/Script/DetectorMeta.cs
@@ -1,21 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DetectorMeta : MonoBehaviour
 {
     [SerializeField]
     GameObject pantallaFinal;
 
-  
-    private void OnTriggerEnter(Collider other)
+    [SerializeField]
+    TextMeshProUGUI textLabelTime;
+
+    float tiempoDeLaPartida = 0.0f;
+    bool estaJugando = true;
+
+    private void Update()
+    {
+        if (estaJugando == true)
         {
-            if (other.tag == "Player")
-            {
-                Debug.Log("Has llegado a la meta");
-                pantallaFinal.SetActive(true);
-                other.GetComponent<MovimientoJugador>().enabled = false;
-            }
+            tiempoDeLaPartida = tiempoDeLaPartida + Time.deltaTime;
         }
-   
+    } 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            //Debug.Log("Has llegado a la meta");
+            pantallaFinal.SetActive(true);
+            other.GetComponent<MovimientoJugador>().enabled = false;
+            //Debug.Log(tiempoDeLaPartida);
+            estaJugando = false;
+            textLabelTime.text = tiempoDeLaPartida.ToString();
+        }
+    }
 }
