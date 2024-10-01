c076f6d40603ba75fc57fd77b6762d9265ba37cf
LilianXeyman <elisa0101232478@gmail.com>
2024-10-01 17:29:45 +0100

    Contador de vidas
    Puse el contador de vidas + la pantalla de has muerto
    Intento hacer el botón de reintentar pero es con distintas escenas

--- Assets/Script/CuentaChoques.cs
+++ Assets/Script/CuentaChoques.cs
@@ -11,7 +11,15 @@ public class CuentaChoques : MonoBehaviour
     [SerializeField]
     TextMeshProUGUI CuentaDeChoques;
 
+    [SerializeField]
+    GameObject pantallaMuerte;
+
+    [SerializeField]
+    TextMeshProUGUI numeroDeVidas;
+
     int numeroChoques = 0;
+    int numeroVidas = 8;
+    int numeroVidas2 = 8;
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -20,7 +28,18 @@ public class CuentaChoques : MonoBehaviour
             numeroChoques = numeroChoques + 1;
             Debug.Log("He chocado " + numeroChoques + " veces");
         }
-    }   
+        if (numeroChoques == numeroVidas)
+        {
+            pantallaMuerte.SetActive(true);
+            gameObject.GetComponent<MovimientoJugador>().enabled = false;
+        }
+        if (collision.gameObject.tag == "Obstaculo")
+        {
+            numeroVidas2 = numeroVidas2 - 1;
+            Debug.Log("Me quedan " + numeroVidas + " vidas");
+        }
+        numeroDeVidas.text = numeroVidas2.ToString();
+    }
     private void OnTriggerEnter(Collider other)
     { 
         if (other.tag == "Meta")
