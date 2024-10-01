7e1915910ec146a0a320da8a208490ced8391af0
LilianXeyman <elisa0101232478@gmail.com>
2024-10-01 11:14:51 +0100

    Enemigos?
    Quería añadir enemigos patrullando pero me parece muy complejo.
    No sé como hacer que se paren en un punto. Busqué y era con algo del Vector3.

---
+++ Assets/Script/PatrullaEnemigo.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrullaEnemigo : MonoBehaviour
+{
+   /* [SerializeField]
+    float velocidadTranslacion = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0.0f, 0.0f,-Time.deltaTime*velocidadTranslacion, Space.World);
+        
+        if(gameObject.Tag=="Enemy"==(0.0f,0.0f,-5.02)
+        {
+            transform.Translate(-Time.deltaTime * velocidadTranslacion, 0.0f, 0.0f, Space.World);
+        }
+    }*/
+}
